6b865e74bf17b2d5325d40b12a09f257bb645f71
deepak <deepak.n.0603@gmail.com>
2026-03-13 13:33:19 +0530

    change in code

--- calculator.py
+++ calculator.py
@@ -5,31 +5,31 @@ class Calculator:
         self.num2 = num2
 
     def add(self):
-        if not isinstance(self.num1, int) and not isinstance(self.num2, int):
+        if not isinstance(self.num1, int) or not isinstance(self.num2, int):
             return "Please... Provide a Number"
         else:
             return self.num1 + self.num2
 
     def sub(self):
-        if not isinstance(self.num1, int) and not isinstance(self.num2, int):
+        if not isinstance(self.num1, int) or not isinstance(self.num2, int):
             return "Please... Provide a Number"
         else:
             return self.num1 - self.num2
 
     def mul(self):
-        if not isinstance(self.num1, int) and not isinstance(self.num2, int):
+        if not isinstance(self.num1, int) or not isinstance(self.num2, int):
             return "Please... Provide a Number"
         else:
             return self.num1 * self.num2
     
     def sqrt(self):
-        if not isinstance(self.num1, int) and not isinstance(self.num2, int):
+        if not isinstance(self.num1, int) or not isinstance(self.num2, int):
             return "Please... Provide a Number"
         else:
             return self.num1 ** self.num2
 
     def divide(self):
-        if not isinstance(self.num1, int) and not isinstance(self.num2, int):
+        if not isinstance(self.num1, int) or not isinstance(self.num2, int):
             return "Please... Provide a Number"
         else:
             return self.num1 / self.num2
--- test_calculator.py
+++ test_calculator.py
@@ -2,20 +2,20 @@ from calculator import Calculator
 
 def test_add():
     calc = Calculator.add(10, 5)
-    assert calc == 15
+    assert calc.add() == 15
 
 def test_sub():
     calc = Calculator.sub(10, 5)
-    assert calc == 5
+    assert calc.sub() == 5
 
 def test_mul():
     calc = Calculator.mul(10, 5)
-    assert calc == 50
+    assert calc.mul() == 50
 
 def test_divide():
     calc = Calculator.divide(10, 5)
-    assert calc == 2
+    assert calc.divide() == 2
 
 def test_sqrt():
     calc = Calculator.sqrt(10, 5)
-    assert calc == 100
+    assert calc.sqrt == 100
